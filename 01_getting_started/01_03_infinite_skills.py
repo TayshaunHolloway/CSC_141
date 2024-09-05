d3c856f9b801ac86add552d09449045dd5f7ff10
TayshaunHolloway <tayshaun.holloway001@albright.edu>
2024-09-05 09:57:02 -0400

    First commit

--- 01_getting_started/01_03_infinite_skills.py
+++ 01_getting_started/01_03_infinite_skills.py
@@ -1,1 +1,3 @@
-# I haven't really done any type of coding before but I'm looking forward to learning code so I can develop video games. 
+# I haven't really done any type of coding before but I'm looking forward to learning code so I can develop video games.
+# Over time I want to try making mods for other games and join the modding community and make games more enjoyable for others to play.
+# Eventually, I want to own a gaming company that can make games that are enjoyable for gamers across the globe.
